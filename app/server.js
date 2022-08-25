3dc9ad91afdf253266f363fbddae081b934ed64b
indrekmae <32834736+indrekmae@users.noreply.github.com>
2022-08-25 15:24:18 +0300

    Update server.js

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World! <br/> This is dockerized node 12 based javascript website!')
+  res.send('Hello World! This is dockerized node 12 based javascript website!')
 })
 
 app.listen(port, () => {
